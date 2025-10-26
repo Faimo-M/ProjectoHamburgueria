d580d1ed74351bf1b970e8fae61535280c6db71b
Faimo-M <faimom258@gmail.com>
2025-10-26 13:33:17 +0200

    ja estou iniciando os trabalhos com o js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./**/*.{html,js,jsx,ts,tsx}"],
     theme: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
         extend: {
          backgroundImage: {
             "home": "url('/assets/bg.png')"
